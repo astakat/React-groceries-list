eac31ddb0652fb95feecd93a25e4748eac78021c
Asta Kat <astakat@gmail.com>
2022-04-05 17:53:04 +0200

    deel 1

---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class List extends React.Component {
+    render() {
+        return <ul>
+          <li>Toothpaste</li>
+          <li>Tea</li>
+          <li>Bread</li>
+        </ul>;
+    }
+      
+} 
+        
+    
+
+export default List;
