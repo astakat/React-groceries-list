3014e4f5acd7cef275088edcacba68e156583ee9
Asta Kat <astakat@gmail.com>
2022-04-08 10:06:57 +0200

    deel 2

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ListItem from './ListItem'
 
 class List extends React.Component {
     render() {
         return <ul>
-          <li>Toothpaste</li>
-          <li>Tea</li>
-          <li>Bread</li>
+          <ListItem title= "Toothpaste"/>
+          <ListItem title= "Tea"/>
+          <ListItem title= "Bread"/>
+          <ListItem title= "Almond milk"/>
         </ul>;
     }
       
---
+++ src/components/ListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ListItem(props) {
+    return(
+        <div>
+            {props.title}
+        </div>
+    );
+}
+
+export default ListItem;
